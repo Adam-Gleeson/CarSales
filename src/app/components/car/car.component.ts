f281d1387f4017de09a0d2c23ac99e5407558243
Adam-Gleeson <s00221503@mail.itsligo.ie>
2023-04-15 16:41:10 +0100

    HTML Updated

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Input } from '@angular/core';
+import { CarApiService } from 'src/app/car-api.service';
+import { ICar } from 'src/app/car';
+
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
 export class CarComponent {
+@Input() carData:ICar | undefined;
 
+carImageWidth: number = 300;
+private _carAPIService: CarApiService;
+  
+constructor(private carAPIService: CarApiService) {
+  this._carAPIService = carAPIService;
+}
+
+
+//car-components-ts.txt Code
+deleteCar(carId:string) { 
+  this._carAPIService.delCarDetails(carId).subscribe(result =>
+    { 
+      console.log(result);
+    });
+}
 }
